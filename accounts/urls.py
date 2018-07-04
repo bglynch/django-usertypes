68e7edd54407a68694a730b55a2e12fa9fbc929b
Brian Lynch <bglynch17@gmail.com>
2018-07-04 10:08:35 +0000

    User functionality added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import login, register
+from .views import login, register, logout, profile
 
 urlpatterns = [
     path('login/', login, name="login"),
     path('register/', register, name="register"),
+    path('logout/', logout, name="logout"),
+    path('profile/', profile, name="profile"),
 ]
--- djangosuperusertypes/settings.py
+++ djangosuperusertypes/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.contexts.get_weather',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
