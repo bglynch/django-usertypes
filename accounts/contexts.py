117b90d0c6d3135c344bdeb4d8911913e66a0ab3
Brian Lynch <bglynch17@gmail.com>
2018-07-04 08:22:10 +0000

    initial commit

---
+++ accounts/contexts.py
@@ -0,0 +1,3 @@
+
+def get_weather(request):
+    return  {'weather': 'hot as hell', 'temperature':29}
---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import UserLoginForm, UserRegistrationForm, ProfileRegistrationForm
+
+# Create your views here.
+def get_home(request):
+    return render(request, "index.html")
+    
+def login(request):
+    form = UserLoginForm()
+    return render(request, "accounts/login.html", {'form': form})
+    
+def register(request):
+    form = UserRegistrationForm()
+    profile_form = ProfileRegistrationForm()
+    return render(request, "accounts/register.html", {'form': form, 'profile_form':profile_form})
